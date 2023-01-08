353f0cde334b018f00d77bb0cbab935f9c3dcdf4
Edgeeee11 <arsenykurenkov@gmail.com>
2023-01-08 15:56:49 +0300

    Homework

--- main.cpp
+++ main.cpp
@@ -9,7 +9,6 @@ int squ(int x)
   s = pow(int(q), 2);
   return s;
 }
-
 int squ_count(int x, int y, int z)
 {
   int count = 0;
@@ -19,48 +18,36 @@ int squ_count(int x, int y, int z)
   {
     count++;
   }
-
   temp = squ(y);
-
   if (temp == y)
   {
     count++;
   }
-
   temp = squ(z);
-
   if (temp == z)
   {
     count++;
   }
-
   return count;
 }
-
 void squ_count(int x, int y, int z, int *c) 
 { 
 *c = squ_count(x, y, z); 
 }
-
 void squ_count(int x, int y, int z, int &c)
 { 
 c = squ_count(x, y, z);
 }
-
 int main()
 {
   int x = 111;
   int y = 121;
   int z = 144;
   int c = 0;
-  
   printf("%i\n", squ_count(x, y, z));
   squ_count(x, y, z, &c);
-
   printf("%i\n", c);
   squ_count(x, y, z, c);
-  
   printf("%i\n", c);
-  
   return 1;
 }
